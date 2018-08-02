7e1403678307b1b1d4c42e2a78fb8312cb399f08
ajai1 <emailtoajai@gmail.com>
2018-08-02 22:47:03 +0530

    Added Create Function

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
 const mongoose = require('mongoose');
-
+//Connecting to the DB
 mongoose.connect('mongodb://localhost/mongo-exercises');
-
+//Schema References
 const courseSchema = new mongoose.Schema({
     name: String,
     author: String,
     tags: [String],
     isPublished: Boolean,
     price: Number,
-    date:Date
+    date:{type:Date, default:Date.now}
 });
-
+//Model is created to do the operations on the DB
 const Course = mongoose.model('courses',courseSchema);
 
-async function getPublishedCourse(isPublished){
-    const course = await Course.find({isPublished:isPublished, tags:'backend'})
-        .limit(10)
-        .sort({name:1})
-        .select({name:1, tags:1});
-    console.log(course);
-}
+//read functions{
+    async function getPublishedCourse(isPublished){
+        //get only the published Course
+        const course = await Course.find({isPublished:isPublished, tags:'backend'})
+            .limit(10)
+            .sort({name:1})
+            .select({name:1, tags:1});
+        console.log(course);
+    }
+    
+    async function getPublishedCourseBasedOnPrice(){
+        //get Published Course on sorting the Price in Desc
+        const course = await Course
+            .find({isPublished:true})
+            .or([{tags:'backend'}])
+            .or([{tags:'frontend'}])
+            .sort({price: -1})
+            .select({name:1, author:1, tags:1, price:1});
+        console.log(course);
+    }
+    
+    async function getPublishedCourseMoreThan15(){
+        //get Published Course which has the price more than or equal to 15 || the name containing 'by' in it
+        const course = await Course
+            .find({isPublished:true})
+            .or([
+                {price: {$gte: 15}},
+                {name: /.*by*./i}
+            ])
+            .select({name:1, author:1, tags:1, price:1});
+        console.log(course);    
+    }
+    getPublishedCourseMoreThan15();
+    //getPublishedCourse(true);
+    //getPublishedCourseBasedOnPrice();
+//}
 
-async function getPublishedCourseBasedOnPrice(){
-    const course = await Course
-        .find({isPublished:true})
-        .or([{tags:'backend'}])
-        .or([{tags:'frontend'}])
-        .sort({price: -1})
-        .select({name:1, author:1, tags:1, price:1});
-    console.log(course);
-}
+// create function{
+    async function createCourse(){
+        const course = new Course({
+            name: 'MEAN Stack Course',
+            author: 'Ajai Kumar',
+            tags: [ 'frontend', 'backend' ],
+            price: 22,
+            isPublished: true
+        });
 
-async function getPublishedCourseMoreThan15(){
-    const course = await Course
-        .find({isPublished:true})
-        .or([
-            {price: {$gte: 15}},
-            {name: /.*by*./i}
-        ])
-        .select({name:1, author:1, tags:1, price:1});
-    console.log(course);    
-}
+        const result = await course.save();
+        console.log(result);
+    }
 
-getPublishedCourseMoreThan15();
-//getPublishedCourse(true);
-//getPublishedCourseBasedOnPrice();
+    //createCourse();
+//}
+
